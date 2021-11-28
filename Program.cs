3279ea0ba9abdb61b6a4d8249b80381709dd17f9
Radoslav304 <radoslavcabadaj304@gmail.com>
2021-11-28 10:38:29 +0100

    Open-Lab-10.02

--- Program.cs
+++ Program.cs
@@ -5,18 +5,37 @@ namespace Open_Lab__10._00
     class Program
     {
 
-        static void Main()
+        static void Main(string[] args)
         {
             Book Lotr = new Book();
-            Lotr.title = "LOTR";
-            Console.WriteLine(Lotr.title);
+            Lotr.Title =("Janko Hraško");
+            Lotr.Pages =("12");
+            Lotr.Category =("fairy tale");
+            Lotr.Autor =("Mária Ďuríčková");
+            Lotr.ReleaseDate =("3.6.2014");
         }
             class Book
         {
-            public string title;
+            private string title;
             private int pages;
+            private string category;
+            private string autor;
+            private int releaseDate;
+
+            public void Title(string newTitle) { title = newTitle; }
+            public void Pages(int newPages) { pages = newPages; }
+            public void Category(string newCategory) { category = newCategory; }
+            public void Autor(string newAutor) { autor = newAutor; }
+            public void ReleaseDate(int newReleaseDate) { releaseDate = newReleaseDate; }
         }
         
-        
+            public void Write()
+        {
+            Console.WriteLine(title);
+            Console.WriteLine(pages);
+            Console.WriteLine(category);
+            Console.WriteLine(author);
+            Console.WriteLine(releaseDate);
+        }
     }
 }
